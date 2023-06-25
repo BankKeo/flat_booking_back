29e7a3f369deefde48cef6d551bcaed1ec5273b9
Bank <Keonakhone1998@gmail.com>
2023-06-25 18:19:05 +0700

    added validation

--- routes/flatRouter.js
+++ routes/flatRouter.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const flatCtrl = require("../controllers/flatCtrl");
 const auth = require("../middleware/auth");
+const authAdmin = require("../middleware/authAdmin");
 
 router.post("/getFlat/:id", flatCtrl.fineOne);
 router.get("/getFlats", flatCtrl.fineMany);
-router.post("/create", auth, flatCtrl.craete);
-router.put("/update/:id", auth, flatCtrl.update);
-router.delete("delete/:id", auth, flatCtrl.delete);
+router.post("/create", auth, authAdmin, flatCtrl.craete);
+router.put("/update/:id", auth, authAdmin, flatCtrl.update);
+router.delete("delete/:id", auth, authAdmin, flatCtrl.delete);
 
 module.exports = router;
