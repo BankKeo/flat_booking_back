713614580bad06848635a05c529ce11fe89ffa96
Bank <Keonakhone1998@gmail.com>
2023-06-25 12:57:52 +0700

    find flat

--- controllers/flatCtrl.js
+++ controllers/flatCtrl.js
@@ -1,6 +1,27 @@
 const Flats = require("../models/flatModel");
 
 const flatCtrl = {
+  fineOne: async (req, res) => {
+    try {
+      const flatOwner = await Flats.findById(req.params.id);
+      if (!flatOwner) {
+        return res.json({ msg: "ບໍ່ພົບຫ້ອງແຖວນີ້ນີ້" });
+      } else {
+        return res.json(flatOwner);
+      }
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  fineMany: async (req, res) => {
+    try {
+      const flatOwner = await Flats.find();
+
+      return res.json(flatOwner);
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
   craete: async (req, res) => {
     try {
       const { flatName, image, tel, village, district, province, googleMapLink } = req.body;
--- routes/flatRouter.js
+++ routes/flatRouter.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const flatCtrl = require("../controllers/flatCtrl");
 const auth = require("../middleware/auth");
 
+router.post("/getFlat/:id", flatCtrl.fineOne);
+router.get("/getFlats", flatCtrl.fineMany);
 router.post("/create", auth, flatCtrl.craete);
 
 module.exports = router;
